30f20678c6da7c40281e9f2f4ff423e98ff8fb96
thomas <psychoid@hotmail.fr>
2020-07-09 10:55:41 +0200

    creation de database_structure

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import database_creation as dc
 
 ###############################################################################
 
-""" We create the class Database for more flexibity. Thanks to that we are less
-independant from MySQL 8.0 """
+"""  """
 
 ###############################################################################
 
 def main():
 
-    
+
 
 
 
