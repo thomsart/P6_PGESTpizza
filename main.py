e37c9ea37097efa2274e4dc185261228024c4983
thomas <psychoid@hotmail.fr>
2020-07-09 10:01:20 +0200

    creation de l'env avec le .gitignore et ajout du requirements.txt

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import database_creation as dc
+
+###############################################################################
+
+""" We create the class Database for more flexibity. Thanks to that we are less
+independant from MySQL 8.0 """
+
+###############################################################################
+
+def main():
+
+    
+
+
+
+    return
+
+if __name__ == "__main__":
+
+    main()
